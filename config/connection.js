9bcdae790949a1cc4688597bfaec57f79b46a262
Quin Eddy <qkeddy@gmail.com>
2022-05-05 22:15:41 -0400

    Created connection to Mongo. Set up model scaffolding.

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+
+// Require supporting NPM modules
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+mongoose.connect('mongodb://localhost:27017/socialDB', {
+
+    // Set options for compatibility with versions < Mongoose 6x
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    
+});
+
+// Export connection
+module.exports = mongoose.connection;
---
+++ models/index.js
@@ -0,0 +1,7 @@
+// Import models
+const Reaction = require('./Reaction');
+const Thought = require('./Thought');
+const User = require('./User');
+
+// Export models
+module.exports = { Reaction, Thought, User };
